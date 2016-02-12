cba61c23cfc704910a28b8fa30cd2e27e68ee953
hold6 <ertkeasrghroe@asdfg.com>
2016-02-12 16:27:41 +0100

    2

---
+++ Assets/RockPickUp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockPickUp : MonoBehaviour {
+
+	private Teacher.ResourceController ResourceController;
+	public int AmountOfRock = 10;
+
+	void Awake()
+	{
+		ResourceController = GameObject.Find("ResourceController").GetComponent<Teacher.ResourceController>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player") // if the hitting object has a player tag
+		{
+			ResourceController.Rock += AmountOfRock; // Add wood to resourcecontroller
+			Destroy(gameObject);  // Destroy itself after it has been picked up
+		}
+	}
+}
